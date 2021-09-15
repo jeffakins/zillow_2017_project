19a5a49e894b88005e803b6ed4fd0ccbe43c7f24
Jeff Akins <jeffakins21@gmail.com>
2021-09-15 02:17:48 -0700

    Final zillow

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 - Clearly show the distribution of tax rates for each county.
 
 ## Executive Summary
-- 
-- 
+- After modeling the zillow data using four features (bedrooms, bathrooms, square footage, and 2016 average home price by zip code), the OLS and Lasso + Lars produced the best results with a 19% improvement over the baseline model (when comparing validation data). Therefore, these models do show that they could be used to produce a prediction for home values; however, the error is still high at over $180,000 RSME. Additional refining would need to be done in order to use this model as a reliable predictor of home values. 
+
 
 ## Deliverables
 - A report in the form of a presentation, verbal supported by slides.
@@ -51,11 +51,26 @@
 - Deliver 
 
 ### Acquire
+- I created a series of functions to acquire and clean the zillow data, which are located in the zillow_wrangle.py file. They take in all of the single unit properties (code 261 from the propertylandusetypeid column on the properties_2017 table in the zillow data set) that were sold between 1 May and 1 Sept of 2017 (based on the predictions_2017 table) into a Pandas dataFrame.
+- I then add on a column for zipcodes and average home price per zipcode, as well as the county and sate name based on the FIPS code.
 
 ### Prep
+- Top and Bottom 20% of outliers were removed.
+- Uneeded columns were removed
+- The remaining data, aside from the target, was scaled using a min-max scaler
 
 ### Explore
+- Scatterplots with trend lines are used to look for obvious coorelations
+- Boxplots are used to show the distributions
+- A heatmap was generated to show correlation strength between features
+- Statistical tests were used to confirm correlation between the target and other features
 
 ### Model & Evaluate
+- The following models were used:
+ - Baseline (using mean)
+ - Ordinary Least Squares
+ - LASSO + LARS (alpha = 1)
+ - Generalized Linear Model (power = 1: Poisson Distribution)
 
 ## Conclusion
+- The OLS and Lasso + Lars produced the best results with a 19% improvement over the baseline model (comparing validation data). Therefore, these models do show that they could be used to produce a prediction for home values; however, the error is still high at over $180,000 RSME. Additional refining would need to be done in order to use this model as a reliable predictor of home values.
--- zillow_models.py
+++ zillow_models.py
@@ -7,39 +7,203 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import explained_variance_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #---------------Baseline---------------------------------------------------------------------
 
-def baseline(y):
+def baseline(y_train, y_validate):
     '''Function that will create a baseline prediction'''
     # Turn y into a dataframe
-    y = pd.DataFrame(y)
-
-    # Predict mean
-    pred_mean = y.iloc[:,1].mean()
-    y['pred_mean'] = pred_mean
-    
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
+    # Predict mean:
+        #Train
+    pred_mean_train = y_train.tax_value.mean()
+    y_train['pred_mean'] = pred_mean_train
+        #Validate
+    pred_mean_validate = y_validate.tax_value.mean()
+    y_validate['pred_mean'] = pred_mean_validate
     # Find the Root Mean Squared Error
-    rmse_y = mean_squared_error(y.iloc[:,1], y.pred_mean) ** .5
+        #Train
+    rmse_y_train = mean_squared_error(y_train.tax_value, y_train.pred_mean) ** .5
+        #Validate
+    rmse_y_validate = mean_squared_error(y_validate.tax_value, y_validate.pred_mean) ** .5
 
-    print("RMSE using mean: ", round(rmse_y, 2))
+    print("Baseline RMSE using Mean\nTrain/In-Sample: ", round(rmse_y_train, 2), 
+      "\nValidate/Out-of-Sample: ", round(rmse_y_validate, 2))
 
-    return y
+    return y_train, y_validate
 
 
 #---------------Linear Regression (OLS)------------------------------------------------------
 
+def linear_regression(X_train, y_train, X_validate, y_validate):
+    '''Function to perform a linear regression on our data'''
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
+    # Create
+    lm = LinearRegression(normalize=True)
+    # Fit
+    lm.fit(X_train, y_train.tax_value)
+    # Predict
+    y_train['pred_lm'] = lm.predict(X_train)
 
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.pred_lm) ** (1/2)
 
+    # predict validate
+    y_validate['pred_lm'] = lm.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.pred_lm) ** (1/2)
+
+    print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", round(rmse_train, 2), 
+        "\nValidation/Out-of-Sample: ", round(rmse_validate, 2))
+    return rmse_validate, y_validate
+    
 
 #---------------LassoLars--------------------------------------------------------------------
 
+def lasso_lars(X_train, y_train, X_validate, y_validate):
+    '''LassoLars regression for Zillow'''
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
 
+    # create the model object
+    lars = LassoLars(alpha=1)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series!
+    lars.fit(X_train, y_train.tax_value)
+
+    # predict train
+    y_train['pred_lars'] = lars.predict(X_train)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.pred_lars) ** (1/2)
+
+    # predict validate
+    y_validate['pred_lars'] = lars.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.pred_lars) ** (1/2)
+
+    print("RMSE for Lasso + Lars\nTraining/In-Sample: ", round(rmse_train, 2), 
+        "\nValidation/Out-of-Sample: ", round(rmse_validate, 2))
+    return y_validate
 
 
 #---------------Tweedie Regressor (GLM)------------------------------------------------------
 
+def tweedie_regressor(X_train, y_train, X_validate, y_validate):
+    '''Tweedie regressor for zillow data'''
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
 
+    # create the model object
+    glm = TweedieRegressor(power=1, alpha=0)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    glm.fit(X_train, y_train.tax_value)
+
+    # predict train
+    y_train['pred_glm'] = glm.predict(X_train)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.pred_glm) ** (1/2)
+
+    # predict validate
+    y_validate['pred_glm'] = glm.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.pred_glm) ** (1/2)
+
+    print("RMSE for GLM using Tweedie, power=1 & alpha=0\nTraining/In-Sample: ", round(rmse_train, 2), 
+        "\nValidation/Out-of-Sample: ", round(rmse_validate, 2))
+    return y_validate
 
 
 #---------------Polynomial Regression--------------------------------------------------------
+
+
+
+#---------------Compare Regression--------------------------------------------------------
+def make_metric_df(y, y_pred, model_name, metric_df):
+    if metric_df.size ==0:
+        metric_df = pd.DataFrame(data=[
+            {
+                'model': model_name,
+                'RMSE_validate': round(mean_squared_error(
+                    y,
+                    y_pred) ** .5),
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }])
+        return metric_df
+    else:
+        return metric_df.append(
+            {
+                'model': model_name, 
+                'RMSE_validate': round(mean_squared_error(
+                    y,
+                    y_pred) ** .5),
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }, ignore_index=True)
+
+#---------------Compare Regression--------------------------------------------------------
+def model_compare(X_train, y_train, X_validate, y_validate):
+    '''Function to compare regression models'''
+    # create the metric_df as a blank dataframe
+    metric_df = pd.DataFrame()
+
+    # Baseline
+    y_train_b, y_validate_b = baseline(y_train, y_validate)
+        # make our first entry into the metric_df with mean baseline
+    metric_df = make_metric_df(y_validate_b.tax_value,
+                            y_validate_b.pred_mean,
+                            'mean_baseline',
+                            metric_df)
+    # OLS
+    rmse_validate, y_validate_ols = linear_regression(X_train, y_train, X_validate, y_validate)
+
+    metric_df = metric_df.append({
+    'model': 'OLS Regressor', 
+    'RMSE_validate': round(rmse_validate, 0),
+    'r^2_validate': explained_variance_score(y_validate_ols.tax_value, y_validate_ols.pred_lm)}, ignore_index=True)
+
+    # LassoLars
+    y_validate_ll = lasso_lars(X_train, y_train, X_validate, y_validate)
+    metric_df = make_metric_df(y_validate_ll.tax_value,
+               y_validate_ll.pred_lars,
+               'lasso_alpha_1',
+               metric_df)
+
+    # Tweedie regressor
+    y_validate_t = tweedie_regressor(X_train, y_train, X_validate, y_validate)
+    metric_df = make_metric_df(y_validate_t.tax_value,
+                y_validate_t.pred_glm,
+                'glm_poisson',
+                metric_df)
+
+    return metric_df
+
+#---------------plot Regression--------------------------------------------------------
+def plot_baseline(y_train, y_validate):
+    y_train, y_validate = baseline(y_train, y_validate)
+    # plot to visualize actual vs predicted. 
+    plt.hist(y_train.tax_value, color='blue', alpha=.5, label="Samples")
+    plt.hist(y_train.pred_mean, bins=1, color='red', alpha=.5, rwidth=1000, label="Mean")
+    #plt.xlabel("Home Value")
+    #plt.ylabel("not sure")
+    plt.legend()
+    plt.show()
+    return None
+
+
+#---------------plot Regression--------------------------------------------------------
